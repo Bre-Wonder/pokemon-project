d179521ff39e8b38843492bb88064765dc8dde01
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2022-11-13 16:22:30 -0800

    Add files via upload

---
+++ simple-js-app/js/script.js
@@ -0,0 +1,17 @@
+alert('Hello World');
+
+let favoriteFood = 'Pinneaple';
+document.write(favoriteFood);
+
+let myName = 'Bre';
+myName = 'Zach';
+document.write(myName);
+
+let simpleAddition = 9 + 5;
+document.write(simpleAddition);
+
+let size = 100;
+size = 200;
+let doubleSize = size * 10;
+let miniSize = (doubleSize * 2) - (size / 2);
+document.write(miniSize);
