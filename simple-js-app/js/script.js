609e958c565357c68f35cc83f8836300288a1f3b
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2022-11-21 20:51:51 -0800

    added pokemonList with array to script.js

--- simple-js-app/js/script.js
+++ simple-js-app/js/script.js
@@ -1,17 +1,5 @@
-alert('Hello World');
-
-let favoriteFood = 'Pinneaple';
-document.write(favoriteFood);
-
-let myName = 'Bre';
-myName = 'Zach';
-document.write(myName);
-
-let simpleAddition = 9 + 5;
-document.write(simpleAddition);
-
-let size = 100;
-size = 200;
-let doubleSize = size * 10;
-let miniSize = (doubleSize * 2) - (size / 2);
-document.write(miniSize);
+let pokemonList=[
+    {name: 'Squirtle', height: .5, type: 'water'},
+    {name: 'Pikachu', height: .4, type: 'electric'}, 
+    {name: 'Oddish', height: .5, type: 'grass'}
+];
