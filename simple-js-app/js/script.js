76f28e28ee6e935d35be714ec27000231dc6eddc
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2022-12-03 18:03:50 -0800

    changing sytax to find possible errors - js file

--- simple-js-app/js/script.js
+++ simple-js-app/js/script.js
@@ -1,13 +1,13 @@
 let pokemonList=[
     {name: 'Squirtle', height: .5, type: 'water'},
     {name: 'Pikachu', height: .4, type: 'electric'}, 
-    {name: 'Oddish', height: .5, type: 'grass'}
+    {name: 'Oddish', height: .5, type: 'grass'},
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write('<p>' + pokemonList[i].name + '\'s height is: ' + pokemonList[i].height + '</p>')
+    document.write('<p>' + pokemonList[i].name + '\'s height is: ' + pokemonList[i].height + '</p>');
     if (pokemonList[i].height < .5) {
-        document.write(' - You are so petite!'+ '<p></p>');
+        document.write(' - You are so petite!');
     }
 }
 
@@ -15,7 +15,7 @@ function divide(dividend, divisor) {
     if(divisor === 0){
         return 'You\'re trying to divide by zero.'
     }else{
-        let result = dividend/divisor;
+        let result = dividend / divisor;
         return result;
     }
     }
