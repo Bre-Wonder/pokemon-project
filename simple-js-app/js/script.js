de5efb704505ca9091071d21a79e1bfc4412b443
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2022-12-03 16:11:34 -0800

    add for loop and condition to js file

--- simple-js-app/js/script.js
+++ simple-js-app/js/script.js
@@ -3,3 +3,14 @@ let pokemonList=[
     {name: 'Pikachu', height: .4, type: 'electric'}, 
     {name: 'Oddish', height: .5, type: 'grass'}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + '\'s height is: ' + pokemonList[i].height)
+    if (pokemonList[i].height < .5) {
+        document.write(' - You are so petite!'+ '<p></p>');
+    } else { 
+        document.write('<p></p>');
+    }
+}
+
+
