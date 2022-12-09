8f3f5e2f8f0199fec1cedc7c988d4249a50719e1
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2022-12-08 21:17:01 -0800

    deleted uncessary function in script.js

--- simple-js-app/js/script.js
+++ simple-js-app/js/script.js
@@ -11,14 +11,6 @@ for (let i = 0; i < pokemonList.length; i++) {
     }
 }
 
-function divide(dividend, divisor) {
-    if(divisor === 0){
-        return 'You\'re trying to divide by zero.'
-    }else{
-        let result = dividend / divisor;
-        return result;
-    }
-    }
 
 
 
