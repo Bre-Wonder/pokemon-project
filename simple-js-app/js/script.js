1deae44924a1e1d22a505232b29ad375f1e28a6b
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2022-12-11 18:59:06 -0800

    udpated forEach loop

--- simple-js-app/js/script.js
+++ simple-js-app/js/script.js
@@ -1,24 +1,39 @@
-let pokemonList=[
-    {name: 'Squirtle', 
-    height: .5, 
-    type: 'water'},
-    {name: 'Pikachu', 
-    height: .4, 
-    type: 'electric'}, 
-    {name: 'Oddish', 
-    height: .5, 
-    type: 'grass'},
+let pokemonRepository = (function () {
+    let pokemonList = [
+      {name: 'Squirtle', height: .5, type: 'water'},
+      {name: 'Pikachu', height: .4, type: 'electric'}, 
+      {name: 'Oddish', height: .5, type: 'grass'}
 ];
+    
+function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 
-//Past pokemonList for loop//
-// for (let i = 0; i < pokemonList.length; i++) {
-    //document.write('<p>' + pokemonList[i].name + '\'s height is: ' + pokemonList[i].height + '</p>');
-    //if (pokemonList[i].height < .5) {
-        //document.write(' - You are so petite!');
-    //}
-//}
+function getAll() {
+    return pokemonList;
+  }
 
-pokemonList.forEach(function(pokemon){
+  return {
+    add: add,
+    getAll: getAll
+  };
+
+})();
+
+pokemonRepository.add({name: 'Vuplix', height: 0.6, type: 'fire'});
+
+/* Past pokemonList for loop
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write('<p>' + pokemonList[i].name + '\'s height is: ' + pokemonList[i].height + '</p>');
+    if (pokemonList[i].height < .5) {
+        document.write(' - You are so petite!');
+    }
+} */
+
+let pokemonFunction = pokemonRepository.getAll();
+
+pokemonFunction.forEach(function(pokemon){
     document.write('<p>' + pokemon.name + '\'s height is: ' + pokemon.height + '</p>'); 
     if (pokemon.height < .5) {
         document.write(' - You are so petite!');
